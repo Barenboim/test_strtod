7e7e2b0109b2d017fd732e597b4409b2b8060bd3
Xie Han <63350856@qq.com>
2024-01-03 23:22:01 +0800

    test

--- test_strtod.cc
+++ test_strtod.cc
@@ -48,8 +48,8 @@ static double powersOf10[] = {	/* Table giving binary powers of 10.  Entry */
  */
 
 double
-apple_strtod(string, endPtr)
-    const char *string;		/* A decimal ASCII floating-point number,
+apple_strtod(const char *string, char **endPtr)
+    	/* A decimal ASCII floating-point number,
 				 * optionally preceded by white space.
 				 * Must have form "-I.FE-X", where I is the
 				 * integer part of the mantissa, F is the
@@ -61,13 +61,13 @@ apple_strtod(string, endPtr)
 				 * The "E" may actually be an "e".  E and X
 				 * may both be omitted (but not just one).
 				 */
-    char **endPtr;		/* If non-NULL, store terminating character's
+   		/* If non-NULL, store terminating character's
 				 * address here. */
 {
     int sign, expSign = FALSE;
     double fraction, dblExp, *d;
-    register const char *p;
-    register int c;
+    const char *p;
+    int c;
     int exp = 0;		/* Exponent read from "EX" field. */
     int fracExp = 0;		/* Exponent that derives from the fractional
 				 * part.  Under normal circumstatnces, it is
@@ -309,6 +309,78 @@ static const double __power_of_10[309] = {
 	1.0e305, 1.0e306, 1.0e307, 1.0e308,
 };
 
+static const long double __lpower_of_10[342] = {
+	1.0e0l,   1.0e1l,   1.0e2l,   1.0e3l,   1.0e4l,
+	1.0e5l,   1.0e6l,   1.0e7l,   1.0e8l,   1.0e9l,
+	1.0e10l,  1.0e11l,  1.0e12l,  1.0e13l,  1.0e14l,
+	1.0e15l,  1.0e16l,  1.0e17l,  1.0e18l,  1.0e19l,
+	1.0e20l,  1.0e21l,  1.0e22l,  1.0e23l,  1.0e24l,
+	1.0e25l,  1.0e26l,  1.0e27l,  1.0e28l,  1.0e29l,
+	1.0e30l,  1.0e31l,  1.0e32l,  1.0e33l,  1.0e34l,
+	1.0e35l,  1.0e36l,  1.0e37l,  1.0e38l,  1.0e39l,
+	1.0e40l,  1.0e41l,  1.0e42l,  1.0e43l,  1.0e44l,
+	1.0e45l,  1.0e46l,  1.0e47l,  1.0e48l,  1.0e49l,
+	1.0e50l,  1.0e51l,  1.0e52l,  1.0e53l,  1.0e54l,
+	1.0e55l,  1.0e56l,  1.0e57l,  1.0e58l,  1.0e59l,
+	1.0e60l,  1.0e61l,  1.0e62l,  1.0e63l,  1.0e64l,
+	1.0e65l,  1.0e66l,  1.0e67l,  1.0e68l,  1.0e69l,
+	1.0e70l,  1.0e71l,  1.0e72l,  1.0e73l,  1.0e74l,
+	1.0e75l,  1.0e76l,  1.0e77l,  1.0e78l,  1.0e79l,
+	1.0e80l,  1.0e81l,  1.0e82l,  1.0e83l,  1.0e84l,
+	1.0e85l,  1.0e86l,  1.0e87l,  1.0e88l,  1.0e89l,
+	1.0e90l,  1.0e91l,  1.0e92l,  1.0e93l,  1.0e94l,
+	1.0e95l,  1.0e96l,  1.0e97l,  1.0e98l,  1.0e99l,
+	1.0e100l, 1.0e101l, 1.0e102l, 1.0e103l, 1.0e104l,
+	1.0e105l, 1.0e106l, 1.0e107l, 1.0e108l, 1.0e109l,
+	1.0e110l, 1.0e111l, 1.0e112l, 1.0e113l, 1.0e114l,
+	1.0e115l, 1.0e116l, 1.0e117l, 1.0e118l, 1.0e119l,
+	1.0e120l, 1.0e121l, 1.0e122l, 1.0e123l, 1.0e124l,
+	1.0e125l, 1.0e126l, 1.0e127l, 1.0e128l, 1.0e129l,
+	1.0e130l, 1.0e131l, 1.0e132l, 1.0e133l, 1.0e134l,
+	1.0e135l, 1.0e136l, 1.0e137l, 1.0e138l, 1.0e139l,
+	1.0e140l, 1.0e141l, 1.0e142l, 1.0e143l, 1.0e144l,
+	1.0e145l, 1.0e146l, 1.0e147l, 1.0e148l, 1.0e149l,
+	1.0e150l, 1.0e151l, 1.0e152l, 1.0e153l, 1.0e154l,
+	1.0e155l, 1.0e156l, 1.0e157l, 1.0e158l, 1.0e159l,
+	1.0e160l, 1.0e161l, 1.0e162l, 1.0e163l, 1.0e164l,
+	1.0e165l, 1.0e166l, 1.0e167l, 1.0e168l, 1.0e169l,
+	1.0e170l, 1.0e171l, 1.0e172l, 1.0e173l, 1.0e174l,
+	1.0e175l, 1.0e176l, 1.0e177l, 1.0e178l, 1.0e179l,
+	1.0e180l, 1.0e181l, 1.0e182l, 1.0e183l, 1.0e184l,
+	1.0e185l, 1.0e186l, 1.0e187l, 1.0e188l, 1.0e189l,
+	1.0e190l, 1.0e191l, 1.0e192l, 1.0e193l, 1.0e194l,
+	1.0e195l, 1.0e196l, 1.0e197l, 1.0e198l, 1.0e199l,
+	1.0e200l, 1.0e201l, 1.0e202l, 1.0e203l, 1.0e204l,
+	1.0e205l, 1.0e206l, 1.0e207l, 1.0e208l, 1.0e209l,
+	1.0e210l, 1.0e211l, 1.0e212l, 1.0e213l, 1.0e214l,
+	1.0e215l, 1.0e216l, 1.0e217l, 1.0e218l, 1.0e219l,
+	1.0e220l, 1.0e221l, 1.0e222l, 1.0e223l, 1.0e224l,
+	1.0e225l, 1.0e226l, 1.0e227l, 1.0e228l, 1.0e229l,
+	1.0e230l, 1.0e231l, 1.0e232l, 1.0e233l, 1.0e234l,
+	1.0e235l, 1.0e236l, 1.0e237l, 1.0e238l, 1.0e239l,
+	1.0e240l, 1.0e241l, 1.0e242l, 1.0e243l, 1.0e244l,
+	1.0e245l, 1.0e246l, 1.0e247l, 1.0e248l, 1.0e249l,
+	1.0e250l, 1.0e251l, 1.0e252l, 1.0e253l, 1.0e254l,
+	1.0e255l, 1.0e256l, 1.0e257l, 1.0e258l, 1.0e259l,
+	1.0e260l, 1.0e261l, 1.0e262l, 1.0e263l, 1.0e264l,
+	1.0e265l, 1.0e266l, 1.0e267l, 1.0e268l, 1.0e269l,
+	1.0e270l, 1.0e271l, 1.0e272l, 1.0e273l, 1.0e274l,
+	1.0e275l, 1.0e276l, 1.0e277l, 1.0e278l, 1.0e279l,
+	1.0e280l, 1.0e281l, 1.0e282l, 1.0e283l, 1.0e284l,
+	1.0e285l, 1.0e286l, 1.0e287l, 1.0e288l, 1.0e289l,
+	1.0e290l, 1.0e291l, 1.0e292l, 1.0e293l, 1.0e294l,
+	1.0e295l, 1.0e296l, 1.0e297l, 1.0e298l, 1.0e299l,
+	1.0e300l, 1.0e301l, 1.0e302l, 1.0e303l, 1.0e304l,
+	1.0e305l, 1.0e306l, 1.0e307l, 1.0e308l, 1.0e309l,
+	1.0e310l, 1.0e311l, 1.0e312l, 1.0e313l, 1.0e314l,
+	1.0e315l, 1.0e316l, 1.0e317l, 1.0e318l, 1.0e319l,
+	1.0e320l, 1.0e321l, 1.0e322l, 1.0e323l, 1.0e324l,
+	1.0e325l, 1.0e326l, 1.0e327l, 1.0e328l, 1.0e329l,
+	1.0e330l, 1.0e331l, 1.0e332l, 1.0e333l, 1.0e334l,
+	1.0e335l, 1.0e336l, 1.0e337l, 1.0e338l, 1.0e339l,
+	1.0e340l, 1.0e341l
+};
+
 static double __evaluate_json_number(const char *integer,
 									 const char *fraction,
 									 int exp)
@@ -324,13 +396,15 @@ static double __evaluate_json_number(const char *integer,
 
 	if (*integer != '0')
 	{
-		figures++;
 		mant = *integer - '0';
-		while (isdigit(*++integer) && figures < 18)
+		integer++;
+		figures++;
+		while (isdigit(*integer) && figures < 18)
 		{
-			figures++;
 			mant *= 10;
 			mant += *integer - '0';
+			integer++;
+			figures++;
 		}
 
 		while (isdigit(*integer))
@@ -350,31 +424,42 @@ static double __evaluate_json_number(const char *integer,
 
 	while (isdigit(*fraction) && figures < 18)
 	{
-		figures++;
 		mant *= 10;
 		mant += *fraction - '0';
-
 		exp--;
 		fraction++;
+		figures++;
 	}
 
-	num = mant;
 	if (exp != 0 && figures != 0)
 	{
-		if (exp > 309 - figures)
-			num = INFINITY;
-		else if (exp > 0)
-			num *= __power_of_10[exp];
-		else if (exp > -309)
-			num /= __power_of_10[-exp];
-		else if (exp > -324 - figures)
+		while (exp > 0 && figures < 18)
 		{
-			num /= __power_of_10[-exp - 308];
-			num /= __power_of_10[308];
+			mant *= 10;
+			exp--;
+			figures++;
 		}
-		else
-			num = 0;
+
+		while (exp < 0 && mant % 10 == 0)
+		{
+			mant /= 10;
+			exp++;
+			figures--;
+		}
 	}
+
+	if (exp == 0 || figures == 0)
+		num = mant;
+	else if (exp > 291)
+		num = INFINITY;
+	else if (exp > 0)
+		num = mant * __power_of_10[exp];
+	else if (exp > -309)
+		num = mant / __power_of_10[-exp];
+	else if (exp > -324 - figures)
+		num = mant / __power_of_10[-exp - 308] / __power_of_10[308];
+	else
+		num = 0.0;
 
 	return sign ? -num : num;
 }
@@ -396,22 +481,26 @@ static int __parse_json_number(const char *cursor, const char **end,
 	if (*cursor == '0' && isdigit(cursor[1]))
 		return -2;
 
-	while (isdigit(*++cursor))
-		;
+	cursor++;
+	while (isdigit(*cursor))
+		cursor++;
 
 	if (*cursor == '.')
 	{
-		fraction = ++cursor;
+		cursor++;
+		fraction = cursor;
 		if (!isdigit(*cursor))
 			return -2;
 
-		while (isdigit(*++cursor))
-			;
+		cursor++;
+		while (isdigit(*cursor))
+			cursor++;
 	}
 
 	if (*cursor == 'E' || *cursor == 'e')
 	{
-		sign = (*++cursor == '-');
+		cursor++;
+		sign = (*cursor == '-');
 		if (sign || *cursor == '+')
 			cursor++;
 
@@ -419,14 +508,16 @@ static int __parse_json_number(const char *cursor, const char **end,
 			return -2;
 
 		exp = *cursor - '0';
-		while (isdigit(*++cursor))
+		cursor++;
+		while (isdigit(*cursor) && exp < 2000000)
 		{
-			if (exp < 2000000)
-			{
-				exp *= 10;
-				exp += *cursor - '0';
-			}
+			exp *= 10;
+			exp += *cursor - '0';
+			cursor++;
 		}
+
+		while (isdigit(*cursor))
+			cursor++;
 
 		if (sign)
 			exp = -exp;
@@ -444,7 +535,7 @@ static double __evaluate_json_number2(const char *integer,
 									 const char *fraction,
 									 int exp)
 {
-	unsigned long long mant = 0;
+	long long mant = 0;
 	int figures = 0;
 	double num;
 	int sign;
@@ -455,13 +546,15 @@ static double __evaluate_json_number2(const char *integer,
 
 	if (*integer != '0')
 	{
-		figures++;
 		mant = *integer - '0';
-		while (isdigit(*++integer) && figures < FIG)
+		integer++;
+		figures++;
+		while (isdigit(*integer) && figures < 18)
 		{
-			figures++;
 			mant *= 10;
 			mant += *integer - '0';
+			integer++;
+			figures++;
 		}
 
 		while (isdigit(*integer))
@@ -479,28 +572,44 @@ static double __evaluate_json_number2(const char *integer,
 		}
 	}
 
-	while (isdigit(*fraction) && figures < FIG)
+	while (isdigit(*fraction) && figures < 18)
 	{
-		figures++;
 		mant *= 10;
 		mant += *fraction - '0';
-
 		exp--;
 		fraction++;
+		figures++;
+	}
+
+	if (figures != 0)
+	{
+		while (exp > 0 && figures < 18)
+		{
+			mant *= 10;
+			exp--;
+			figures++;
+		}
+
+		while (exp < 0 && mant % 10 == 0)
+		{
+			mant /= 10;
+			exp++;
+			figures--;
+		}
 	}
 
 	if (exp == 0 || figures == 0)
 		num = mant;
-	else if (exp > 309 - figures)
+	else if (exp > 291)
 		num = INFINITY;
 	else if (exp > 0)
-		num = (long double)mant * __power_of_10[exp];
+		num = mant * __power_of_10[exp];
 	else if (exp > -309)
-		num = (long double)mant / __power_of_10[-exp];
+		num = mant / __power_of_10[-exp];
 	else if (exp > -324 - figures)
-		num = (long double)mant / __power_of_10[-exp - 308] / __power_of_10[308];
+		num = mant / __power_of_10[-exp - 308] / __power_of_10[308];
 	else
-		num = 0;
+		num = 0.0;
 
 	return sign ? -num : num;
 }
@@ -522,22 +631,26 @@ static int __parse_json_number2(const char *cursor, const char **end,
 	if (*cursor == '0' && isdigit(cursor[1]))
 		return -2;
 
-	while (isdigit(*++cursor))
-		;
+	cursor++;
+	while (isdigit(*cursor))
+		cursor++;
 
 	if (*cursor == '.')
 	{
-		fraction = ++cursor;
+		cursor++;
+		fraction = cursor;
 		if (!isdigit(*cursor))
 			return -2;
 
-		while (isdigit(*++cursor))
-			;
+		cursor++;
+		while (isdigit(*cursor))
+			cursor++;
 	}
 
 	if (*cursor == 'E' || *cursor == 'e')
 	{
-		sign = (*++cursor == '-');
+		cursor++;
+		sign = (*cursor == '-');
 		if (sign || *cursor == '+')
 			cursor++;
 
@@ -545,14 +658,16 @@ static int __parse_json_number2(const char *cursor, const char **end,
 			return -2;
 
 		exp = *cursor - '0';
-		while (isdigit(*++cursor))
+		cursor++;
+		while (isdigit(*cursor) && exp < 2000000)
 		{
-			if (exp < 2000000)
-			{
-				exp *= 10;
-				exp += *cursor - '0';
-			}
+			exp *= 10;
+			exp += *cursor - '0';
+			cursor++;
 		}
+
+		while (isdigit(*cursor))
+			cursor++;
 
 		if (sign)
 			exp = -exp;
@@ -570,6 +685,8 @@ static int __parse_json_number2(const char *cursor, const char **end,
 #include <string.h>
 #include <stdlib.h>
 #include <sys/times.h>
+#//include "fast_double_parser.h"
+
 
 int main()
 {
@@ -600,33 +717,45 @@ int main()
 				printf("%.50e, strlen = %lu, nlen = %lu. (Json2)\n", d, len, end - buf);
 			else
 				printf("Error with JSON\n");
-
-	#define N	5000000
-
+/*
+			end = (char *)fast_double_parser::parse_number(buf, &d);
+			if (!end)
+				end = buf;
+	
+			printf("%.50e, strlen = %lu, nlen = %lu. (Fast)\n", d, len, end - buf);*/
+			#define N	100000000
+/*
 			from = times(NULL);
 			for (i = 0; i < N; i++)
 				d = apple_strtod(buf, &end);
 			to = times(NULL);
-			printf("Apple time: %lf, %.20lf\n", (to-from)/100.0, d);
-
+			printf("Apple time: %lf, %.20lf\n", (to-from)/100.0, 0.0);
+*/
 			from = times(NULL);
 			for (i = 0; i < N; i++)
 				d = strtod(buf, &end);
 			to = times(NULL);
-			printf("libc  time: %lf, %.20lf\n", (to-from)/100.0, d);
+			printf("libc  time: %lf, %.20lf\n", (to-from)/100.0, 0.0);
 
 			from = times(NULL);
 			for (i = 0; i < N; i++)
 				__parse_json_number(buf, (const char **)&end, &d);
 			to = times(NULL);
-			printf("Json  time: %lf, %.20lf\n", (to-from)/100.0, d);
+			printf("Json  time: %lf, %.20lf\n", (to-from)/100.0, 0.0);
 
 			from = times(NULL);
 			for (i = 0; i < N; i++)
 				__parse_json_number2(buf, (const char **)&end, &d);
 			to = times(NULL);
-			printf("Json2 time: %lf, %.20lf\n", (to-from)/100.0, d);
-
+			printf("Json2 time: %lf, %.20lf\n", (to-from)/100.0, 0.0);
+	
+/*
+			from = times(NULL);
+			for (i = 0; i < N; i++)
+				fast_double_parser::parse_number(buf, &d);
+			to = times(NULL);
+			printf("Fast  time: %lf, %.20lf\n", (to-from)/100.0, 0.0);
+			*/
 		}
 		else
 			break;
